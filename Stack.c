077fb076f5575d255af10b8c2c8cb8cc8f0a6159
Harsh324 <harshravi324@gmail.com>
2021-12-05 10:07:49 +0530

    Some minor changes done

--- Stack.c
+++ Stack.c
@@ -1,81 +1,124 @@
 #include "Stack.h"
-#include<stdlib.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-int * Stackarray = NULL;
+
+
+int* Stackarray = NULL;
 int top_of_stack = -1;
-
-
 
 void create()
 {
     destroy();
-    Stackarray = (int*)malloc(sizeof(int)*MAX_STACK_SIZE);
+    Stackarray = (int*)malloc(MAX_STACK_SIZE * sizeof(int));
     top_of_stack = -1;
-}
-
-
-void destroy()
-{
-    if(Stackarray != NULL)
-        free(Stackarray);
     return;
 }
 
-
-
-bool push(int Val)
-{
-    if(check_if_full == FALSE)
+void destroy(){
+    if(NULL != Stackarray)
     {
-        Stackarray[++top_of_stack] = Val;
-        return TRUE;
+        free(Stackarray);
+        Stackarray = NULL;
     }
-    return FALSE;
+    return;
 }
 
-
-
-bool pop(int* Ptr)
-{
-    if(check_if_empty == FALSE)
+bool push(int val){
+    if(False == Check_if_full())
     {
-        *Ptr = Stackarray[top_of_stack--];
-        return TRUE;
+        top_of_stack++;
+        Stackarray[top_of_stack] = val;
+        return True;
     }
-    return FALSE;
+    return False;
 }
 
-
-
-bool peek(int* Ptr)
-{
-    if(check_if_empty == FALSE)
+bool pop(int* Ptr){
+    if(False == Check_if_empty())
     {
         *Ptr = Stackarray[top_of_stack];
-        return TRUE;
+        top_of_stack--;
+        return True;
     }
-    return FALSE;
+    return False;
 }
 
+bool peek(int* Ptr){
+    if(False == Check_if_empty())
+    {
+        *Ptr = Stackarray[top_of_stack];
+        //top_of_stack--;
+        return True;
+    }
+    return False;
+}
 
-
-int Len()
+int size()
 {
-    return top_of_stack+1;
+    return top_of_stack + 1;
 }
 
-
-bool check_if_full()
-{
-    if(MAX_STACK_SIZE  == top_of_stack+1)
-        return TRUE;
-    return FALSE;
-}
-
-
-bool check_if_empty()
+bool Check_if_empty()
 {
     if(-1 == top_of_stack)
-        return TRUE;
-    return FALSE;
+        return True;
+
+    return False;
 }
+
+bool Check_if_full()
+{
+    if(MAX_STACK_SIZE == top_of_stack + 1)
+        return True;
+
+    return False;
+}
+
+void Out_Stack()
+{
+    if(True == Check_if_empty())
+        return;
+    printf("***************\n");
+    for(int i = top_of_stack; -1 < i; i--)
+    {
+        printf("at position %d ->  %d\n", i, Stackarray[i]);
+    }
+    printf("***************\n");
+}
+
+
+// Main function is commented out
+/*
+int main()
+{
+    create();
+
+    push(100);
+    push(20);
+    push(40);
+    push(50);
+    push(90);
+    push(95);
+    Out_Stack();
+    int Num = 0;
+
+    if(pop(&Num) == True)
+        printf("%d\n",Num);
+    
+    if(pop(&Num) == True)
+        printf("%d\n",Num);
+    
+    if(peek(&Num) == True)
+        printf("%d\n",Num);
+    
+    if(peek(&Num) == True)
+        printf("%d\n",Num);
+    
+    if(pop(&Num) == True)
+        printf("%d\n",Num);
+    
+    Out_Stack();
+    destroy();
+    return 0;
+}*/
